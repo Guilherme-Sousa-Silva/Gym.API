b1ec1212d97568b74f9107d251065fb9f64f7f47
Bowie <marketing@letscode.com.br>
2024-08-15 00:09:19 -0300

    Criando o projeto de testes e validando dois casos de testes para o a entidade de Gym

--- Gym.Domain/Entities/Gym.cs
+++ Gym.Domain/Entities/Gym.cs
@@ -2,9 +2,28 @@
 {
     public class Gym
     {
+        public Gym(string cnpj, string razaoSocial, string nomeFantasia)
+        {
+            Id = Guid.NewGuid();
+            Cnpj = cnpj;
+            RazaoSocial = razaoSocial;
+            NomeFantasia = nomeFantasia;
+        }
+
         public Guid Id { get; private set; }
         public string Cnpj { get; private set; }
         public string RazaoSocial { get; private set; }
         public string NomeFantasia { get; private set; }
+
+        public bool Validate(Gym gym)
+        {
+            if (gym == null) return false;
+            if (gym.Id == Guid.Empty) return false;
+            if (string.IsNullOrEmpty(gym.Cnpj)) return false;
+            if (string.IsNullOrEmpty(gym.RazaoSocial)) return false;
+            if (string.IsNullOrEmpty(gym.NomeFantasia)) return false;
+
+            return true;
+        }
     }
 }
---
+++ Gym.testes/Domain/GymTests.cs
@@ -0,0 +1,41 @@
+﻿using Gym.Domain.Entities;
+
+namespace Gym.testes.Domain
+{
+    public class GymTests
+    {
+        [Fact]
+        public void CreateGymSuccess()
+        {
+            // Arrange
+            var cnpj = "24123123123";
+            var razaoSocail = "Teste";
+            var nomeFantasia = "Nome Fantasia Teste";
+
+            // Act
+            var gym = new Gym.Domain.Entities.Gym(cnpj, razaoSocail, nomeFantasia);
+
+            // Assert
+            Assert.Equal(gym.Cnpj, cnpj);
+            Assert.Equal(gym.RazaoSocial, razaoSocail);
+            Assert.Equal(gym.NomeFantasia, nomeFantasia);
+        }
+
+        [Fact]
+        public void CreateGymWithCnpjNullShouldReturnError()
+        {
+            // Arrange
+            var cnpj = "";
+            var razaoSocail = "Teste";
+            var nomeFantasia = "Nome Fantasia Teste";
+
+            // Act
+            var gym = new Gym.Domain.Entities.Gym(cnpj, razaoSocail, nomeFantasia);
+            var validate = gym.Validate(gym);
+
+            // Assert
+            Assert.Equal(validate, false);
+            Assert.False(validate);
+        }
+    }
+}
